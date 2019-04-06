15679f1e4a9121a1c67135f98b39152269269e4a
Joshua Nelson <jyn514@gmail.com>
2019-04-06 13:14:19 -0400

    add publishing tools

--- publish.sh
+++ publish.sh
@@ -3,5 +3,6 @@ case "$1" in
 	test) REPO='--repository-url https://test.pypi.org/legacy/';;
 esac
 rm -rf dist
+pip install setuptools twine wheel
 python setup.py bdist_wheel sdist
 twine upload $REPO dist/*
