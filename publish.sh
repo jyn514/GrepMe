53377a103116e3e443a6d9078d9201bd181ebc7d
Joshua Nelson <jyn514@gmail.com>
2019-10-09 23:24:55 -0400

    don't require grepme to already be installed to run tests

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ case "$1" in
 	test) REPO='--repository-url https://test.pypi.org/legacy/';;
 esac
 rm -rf dist
-pip install setuptools twine wheel
+pip install --user --upgrade setuptools twine wheel
 python setup.py bdist_wheel sdist
 twine upload $REPO dist/*
