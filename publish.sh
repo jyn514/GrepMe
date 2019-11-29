e571a892ca82591de1badc375b739593d9d42301
Joshua Nelson <jyn514@gmail.com>
2019-11-29 11:36:49 -0500

    make publish.sh even more automatic

--- publish.sh
+++ publish.sh
@@ -1,8 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -ev
+
 case "$1" in
-	test) REPO='--repository-url https://test.pypi.org/legacy/';;
+	test) shift; REPO='--repository-url https://test.pypi.org/legacy/';;
 esac
 rm -rf dist
+
+VERSION_REGEX="\([1-9]\d*\)\.\([1-9]\d*\)\.\([1-9]\d*\)"
+read major minor patch < <(sed -n "s/VERSION = \"$VERSION_REGEX\"/\1 \2 \3/p" grepme/constants.py)
+case "$1" in
+	major) major=$(( major + 1 ));;
+	minor) minor=$(( minor + 1 ));;
+	patch|*) patch=$(( patch + 1 ));;
+esac
+sed -i "s/VERSION = \"$VERSION_REGEX\"/VERSION = \"$major.$minor.$patch\"/" grepme/constants.py
+
+VERSION="$(grep VERSION grepme/constants.py | cut -d " " -f 3 | tr -d \")"
+git diff grepme/constants.py
+echo "does this diff changing the version to $VERSION look right?"
+read -r REPLY
+[ "$REPLY" = y ] || { echo "aborting"; exit 1; }
+
+git add grepme/constants.py
+git commit -m "bump version number to $VERSION"
+git tag "$VERSION"
+git push --tags && git push
+
 pip install --user --upgrade setuptools twine wheel
 python setup.py bdist_wheel sdist
 twine upload $REPO dist/*
